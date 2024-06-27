7b57761c7f6404d77504ac640b62dfe6e5e49471
Sameer Yadav <142383265+Sameer01-01@users.noreply.github.com>
2024-06-27 22:21:44 +0530

    Add files via upload

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+export default function Header() {
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    };
+
+    return (
+        <div className="bg-slate-900 p-4">
+            <div className="max-w-[1240px] py-[20px] items-center flex justify-between border-black mx-auto">
+                <div className="text-white font-bold text-3xl">Serenity Space</div>
+                {toggle ? (
+                    <AiOutlineClose
+                        onClick={handleToggle}
+                        className="text-white text-2xl md:hidden block"
+                    />
+                ) : (
+                    <AiOutlineMenu
+                        onClick={handleToggle}
+                        className="text-white text-2xl md:hidden block"
+                    />
+                )}
+
+                <ul className="hidden md:flex text-white gap-10">
+                    <li>Home</li>
+                    <li>Get Motivated</li>
+                    <li>Book Appointment</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                </ul>
+
+                <ul
+                    className={`duration-300 md:hidden w-full h-screen text-white fixed bg-black top-[90px] 
+                        ${toggle ? "left-[0]" : "left-[100%]"
+                    }`}
+                >
+                    <li className="p-5">Home</li>
+                    <li className="p-5">Get Motivated</li>
+                    <li className="p-5">Book Appointment</li>
+                    <li className="p-5">About us</li>
+                    <li className="p-5">Contact us</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
